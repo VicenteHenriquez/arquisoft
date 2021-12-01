c60aeda9c804d7c26242a261faf0f49034a48971
Vicente henriquez <vicente.henriquezp@mail.udp.cl>
2021-11-30 21:21:53 -0300

    pruebas1

---
+++ menu.py
@@ -0,0 +1,117 @@
+
+import socket
+import sys
+#create a socket client
+
+
+try:
+    global host
+    global port
+    global s
+    host = '192.168.0.3'
+    port = 5000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+except socket.error as msg:
+    print("Socket creation error: " + str(msg))
+
+def larstr(largo):
+    ceros = 5-len(str(largo))
+    if ceros == 4:
+        largo="0000" + str(largo)
+    elif ceros == 3:
+        largo = "000" + str(largo)
+    elif ceros == 2:
+        largo = "00" + str(largo)
+    elif ceros == 1:
+        largo = "0" + str(largo)
+    return (largo)
+
+def menu_4_options():
+    while True:
+        print("""
+        1. Iniciar sesion
+        2. Registrar
+        3. Salir
+        """)
+        choice = int(input("Elija la opción correspondiente: "))
+        if choice == 1:
+            #iniciar sesion
+            print("Iniciar sesion")
+            correo = input("Correo: ")
+            password = input("Password: ")
+            largo = 5 + 1 +len(correo) + 1 + len(password)
+            texto = larstr(largo) + "adusr" + "1" + correo + " " + password
+            s.send(texto.encode("utf-8"))
+            resp = s.recv(4096)
+            print(resp.decode("utf-8"))
+            respuesta = resp.decode("utf-8")
+            respuesta = respuesta[5:]
+            if respuesta == "True":
+                user = correo
+                menu_sesionini(user)
+            elif respuesta == "False":
+                print("Correo o contraseña incorrecta")
+            else:
+                print("Error")
+        elif choice == 2: #registrar
+            print("Ingrese su nombre: ")
+            nombre = str(input())
+            print("Ingrese su correo: ")
+            correo = str(input())
+            print("Ingrese su password: ")
+            password1 = str(input())
+            print("Ingrese su password nuevamente: ")
+            password2 = str(input())
+            if password1 == password2:
+                largo = 5 + 1 + len(nombre) + 1 + len(correo) + 1 + len(password1)
+                texto = larstr(largo) + "adusr" + "2" + nombre + " " + correo + " " + password1
+                s.send(texto.encode("utf-8"))
+                resp = s.recv(4096)
+                print(resp.decode("utf-8"))
+            pass
+        elif choice == 3:
+            print("Saliendo...Muchas gracias por utilizar nuestros servicios")
+            sys.exit()
+
+def menu_sesionini(user):
+    print("""
+    Bienvenido
+    1. Ver perfil
+    2. Ramos
+    3. Evaluaciones
+    4. Notas
+    5. Salir
+    """)
+    choice = int(input("Elija la opción correspondiente: "))
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        print("Saliendo...Muchas gracias por utilizar nuestros servicios")
+        sys.exit()
+
+def menu_ramos(user):
+    print("""
+    1. Ver ramos
+    2. Agregar ramo
+    3. Eliminar ramo
+    4. Volver
+    """)
+    choice = int(input("Elija la opción correspondiente: "))
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        menu_4_options()
+
+
+menu_4_options()    
---
+++ servicio_admin_user.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+import time
+
+def larstr(largo):
+    ceros = 5-len(str(largo))
+    if ceros == 4:
+        largo="0000" + str(largo)
+    elif ceros == 3:
+        largo = "000" + str(largo)
+    elif ceros == 2:
+        largo = "00" + str(largo)
+    elif ceros == 1:
+        largo = "0" + str(largo)
+    return (largo) 
+ 
+PORT=5000
+conex = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conex.connect(('localhost', PORT))
+
+conex.send(b'00012sinitOKadusr')
+data = conex.recv(4096)
+print(data.decode())
+
+while True:
+    data = conex.recv(4096)
+    print(data.decode())
+    data = data.decode()[5:]
+    data1 = data[0:5]
+    if data1 == "1": #inicio de sesion
+        espacio = data.find(' ') #busca el espacio
+        servicio = data[0:5] #define servicio
+        usuario = data[5:espacio] #define usuario
+        password = data[espacio+1:] #define password
+        print(usuario, password)
+        
+    elif data1 == "2": #registro de usuario
+        pass
+    else:
+        pass
