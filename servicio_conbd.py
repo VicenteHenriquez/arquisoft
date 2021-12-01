7a9f000f7792d900693d98580161f37d58281efc
Vicente henriquez <vicente.henriquezp@mail.udp.cl>
2021-12-01 20:53:51 -0300

    cambios

--- servicio_conbd.py
+++ servicio_conbd.py
@@ -37,9 +37,9 @@ while True:
         resultado = cursor.fetchone()
         #resultado = len(resultado)
         if resultado == None:
-            s.send(b'00006menusf')
+            s.send(b'00006adusrf')
         elif resultado == 1:
-            s.send(b'00006menusv')
+            s.send(b'00006adusrv')
         else:
             pass
     else:
