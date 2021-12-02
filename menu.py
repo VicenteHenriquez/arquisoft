766274aebc79c17eec8ca589aa974439f9b93771
Vicente henriquez <vicente.henriquezp@mail.udp.cl>
2021-12-02 12:45:33 -0300

    prueba

--- menu.py
+++ menu.py
@@ -142,6 +142,8 @@ def menu_ramos(user):
         resp = s.recv(4096)
         respuesta = resp.decode("utf-8")
         respuesta = respuesta[12:]
+        for i in respuesta:
+            print(i)
         print(respuesta)
         print("----------------------------------------------------------")
 
